41189f806a5199c9f95ffa4b04f28850dbc17daa
Keshav-iitm <ae24s021@smail.iitm.ac.in>
2025-06-08 17:25:39 +0530

    Included chaotic case

--- basic_morlet_wavetransform.py
+++ basic_morlet_wavetransform.py
@@ -8,9 +8,12 @@ fs = 100
 duration = 2
 t = np.arange(0,duration,1/fs)
 
-f = 5
-signal = np.sin( 2 * np.pi * f *t)
+#for chaotic case
+T = len(t)
+noise = np.random.rand(T)
 
+f = 5
+signal = np.sin( 2 * np.pi * f *t) + 3*np.sin( 2 * np.pi * 15 *t) #+ 10*noise
 #morlet
 #center frequency of morlet is approx 0.8125
 fc = 0.8125
@@ -30,7 +33,7 @@ print(f'Frequencies vector shape: {frequencies.shape}')
 
 plt.figure(figsize=(12, 6))
 plt.imshow(np.abs(cwt_matrix), extent=[0, duration, frequencies[-1], frequencies[0]],
-           cmap='viridis', aspect='auto')
+           cmap='plasma', aspect='auto')
 plt.colorbar(label='Magnitude')
 plt.xlabel('Time [s]')
 plt.ylabel('Frequency [Hz]')
